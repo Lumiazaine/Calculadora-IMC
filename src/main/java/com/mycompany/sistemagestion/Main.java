8bf383ab3c362f8582d1329294a95c05c095d027
David Luna González <lumiazaine@gmail.com>
2021-10-25 11:11:22 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/sistemagestion/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemagestion;
+
+/**
+ *
+ * @author david
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
